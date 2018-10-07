9ece4a2abb900969cf37f6ecc559be30d6ca52bf
vldmr1 <vldmr.one@gmail.com>
2018-10-07 15:11:48 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+module.exports = function resultConsecutiveLength(array) {
+  let result = 0;
+  const hash = {};
+  array.forEach(function(number) {
+    hash[number] = true;
+  });
+  for (let number in hash) {
+    let current = Number(number),
+        count = 1;
+    while (hash[current + 1]) {
+      count++;
+      current++;
+    }
+  result = Math.max(result, count);
+  }
+ return result;
 }
